83008c9e133d08fa777633d89dc8e5de61057352
Sandeep-622 <sandeep@example.com>
2026-02-26 23:59:17 +0530

    Working

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,30 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
-  -- This ensures the plugin loads early enough to trigger the installs
-  event = { "BufReadPost", "BufNewFile" }, 
-  opts = {
-    ensure_installed = { "lua", "vim", "vimdoc", "markdown", "python", "go" },
-    highlight = { enable = true },
-    indent = { enable = true },
-    auto_install = true,
-  },
-  config = function(_, opts)
-    -- In newer versions, this is the preferred way to call setup
-    local configs = require("nvim-treesitter.configs")
-    configs.setup(opts)
+  config = function()
+    -- New nvim-treesitter API for Neovim 0.11+
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath('data') .. '/site'
+    })
+    
+    -- Install parsers
+    require("nvim-treesitter").install({ "lua", "vim", "vimdoc", "markdown", "python", "go" })
+    
+    -- Enable treesitter highlighting for specific filetypes
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'lua', 'vim', 'markdown', 'python', 'go' },
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+    
+    -- Enable treesitter-based indentation
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'lua', 'python', 'go' },
+      callback = function()
+        vim.bo.indentexpr = 'v:lua.require("nvim-treesitter.indent").get_indent(v:lnum)'
+      end,
+    })
   end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader= " "
+
+-- Enable system clipboard integration
+vim.opt.clipboard = "unnamedplus"
+
+-- Set Python 3 provider
+vim.g.python3_host_prog = "/usr/bin/python3"
